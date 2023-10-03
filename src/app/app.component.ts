65364de1dc86dc82d11a5b6d5073614b28b164dd
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-03 18:01:55 +0200

    Commit
    Conenction Google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from './Services/firebase.service';
+import { AuthService } from './Services/auth.service';
+import { UserCredential } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgAppDating';
+  userCreds!: UserCredential | null;
+  CurrentUser = this.authService.currentUser
+  constructor(
+    // private readonly _fireStoreService: FirebaseService,
+    private readonly authService: AuthService
+  ){
+
+  }
+
+  async actions(type: string, payload?: any) {
+    switch (true) {
+      case type === 'singin':
+        this.userCreds = await this.authService.signinWithGoogle();
+        break;
+      case type === 'singout':
+        await this.authService.singOut();
+        this.userCreds = null;
+        break;
+      default:
+        break;
+    }
+
+  }
+
 }
