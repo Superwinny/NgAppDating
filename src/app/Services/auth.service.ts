6dd0491fc54968429f2704177d94c05046c6b425
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-03 17:43:29 +0200

    Commit

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Auth, GoogleAuthProvider,authState,signInWithPopup, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  currentUser = authState(this._auth)
+
+  constructor(
+    private readonly _auth: Auth
+  ) { }
+
+
+  async signinWithGoogle(){
+    const provider = new GoogleAuthProvider();
+   const userCreds = await signInWithPopup(this._auth, provider)
+  return userCreds;
+  }
+  async singOut(){
+    await signOut(this._auth);
+  }
+
+
 }
