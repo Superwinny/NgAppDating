1a66994cf98cb878da434c6e374605bdadea6392
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-03 14:56:52 +0200

    Commit
    Ajout AngularFire

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'projet-app-dating-angular',
+    appId: '1:222985463441:web:7e7914f1dffd03fc940130',
+    storageBucket: 'projet-app-dating-angular.appspot.com',
+    apiKey: 'AIzaSyDTJUYqrL0XySEBjTDqedIj8WcnBVpt5Bs',
+    authDomain: 'projet-app-dating-angular.firebaseapp.com',
+    messagingSenderId: '222985463441',
+  },
+};
