ddc67348418844e03216c0db9766c4cbe22d6cc4
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-05 17:04:38 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormulairesComponent } from './formulaires/formulaires.component';
 import { PageCoComponent } from './page-co/page-co.component';
+import { PageAppComponent } from './page-app/page-app.component';
 
 const routes: Routes = [
   {path: 'pageco', component: PageCoComponent},
   {path: 'formulaire', component: FormulairesComponent},
+  {path: 'pageapp', component: PageAppComponent},
   {path: '', redirectTo: "pageco", pathMatch: 'full'}
 ];
 
