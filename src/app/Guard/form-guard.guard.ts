839e5b93536e4d49b3287eb461f2157c8c2cab5b
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-14 17:03:50 +0200

    commit

--- src/app/Guard/form-guard.guard.ts
+++ src/app/Guard/form-guard.guard.ts
@@ -8,6 +8,7 @@ export const formGuardGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService)
   const router = inject(Router)
     if (authService.hasFilledForm()) {
+      router.navigate(['/page-app'])
       return true
     } else {
 
