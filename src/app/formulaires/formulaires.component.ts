ac8e312f411821b7c00e534f302fce3a6d654aba
Dos Santos Davide <dos.davide91@gmail.com>
2023-10-17 14:56:47 +0200

    commit

--- src/app/formulaires/formulaires.component.ts
+++ src/app/formulaires/formulaires.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewEncapsulation  } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Storage } from '@angular/fire/storage';
@@ -14,58 +14,53 @@ import { firstValueFrom } from 'rxjs';
 })
 export class FormulairesComponent {
   selectedOption: string = '';
+  selectedDesire: string = '';
+  selectedLookingFor: string = '';
 
-    form: FormGroup = new FormGroup({
-    firstname: new FormControl("", Validators.compose([
-      Validators.required,
+  form: FormGroup = new FormGroup({
+    firstname: new FormControl('', Validators.compose([Validators.required])),
+    birthday: new FormControl('', Validators.compose([Validators.required])),
+    gender: new FormControl('', Validators.compose([Validators.required])),
+    desire: new FormControl('', Validators.compose([Validators.required])),
+    lookingfor: new FormControl('', Validators.compose([Validators.required])),
+    passion: new FormControl('', Validators.compose([Validators.required])),
+    photo: new FormControl('', Validators.compose([Validators.required])),
+  });
 
-    ])),
-    birthday: new FormControl("Dos Santos", Validators.compose([
-      Validators.required,
-    ])),
-    gender: new FormControl("Homme", Validators.compose([
-      Validators.required,
-    ])),
-    desire: new FormControl("Relation", Validators.compose([
-      Validators.required,
-    ])),
-    lookingfor: new FormControl("Femme", Validators.compose([
-      Validators.required,
-    ])),
-    passion: new FormControl("ESport", Validators.compose([
-      Validators.required,
-    ])),
-    photo: new FormControl("", Validators.compose([
-      Validators.required,
-    ])),
-
-  })
   constructor(
     private readonly router: Router,
     private readonly _fireStore: FirebaseService,
     private readonly _auth: AuthService,
-  ) { }
-
-  selectOption(option: string) {
-    this.selectedOption = option;
-  }
+  ) {}
 
   async validateForm() {
-      const user = await firstValueFrom(this._auth.currentUser);
-      if(user?.uid){
-        this._fireStore.addDataUser(this.form.value,user.uid)
-        console.log(this.form.value);
-         this.router.navigate(['/pageapp']);
-      }
-
+    const user = await firstValueFrom(this._auth.currentUser);
+    if (user?.uid) {
+      this._fireStore.addDataUser(this.form.value, user.uid);
+      console.log(this.form.value);
+      this.router.navigate(['/pageapp']);
     }
+  }
 
-    async takepicture(){
-      this._fireStore.takePictureAndUpload();
-    }
-    onSubmitForm(formData: any) {
+  async takepicture() {
+    this._fireStore.takePictureAndUpload();
+  }
 
-      this._auth.markFormAsFilled();
 
-}
+  setGender(gender: string) {
+    this.form.get('gender')?.setValue(gender);
+  }
+
+  setDesire(desire: string) {
+    this.selectedDesire = desire;
+    this.form.get('desire')?.setValue(desire);
+  }
+
+  setLookingFor(lookingFor: string) {
+    this.selectedLookingFor = lookingFor;
+    this.form.get('lookingfor')?.setValue(lookingFor);
+  }
+
+
+
 }
